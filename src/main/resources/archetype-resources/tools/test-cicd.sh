e657133a91b4f2dce4d1abfb469445a90c8adda5
francoisguibourt <francois.guibourt.e@thalesdigital.io>
2022-04-29 17:00:46 +0200

    adding internal tools

---
+++ src/main/resources/archetype-resources/tools/test-cicd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# @author: Punch Team
+# @desc: Script to launch tests with CICD
+
+rc=0
+
+for file in $(find . -path "*/test/*.json" -o -path "*/test/*.txt"); 
+do
+	cmd="docker run puncher-cicd:8.0-dev -T /test/$file"
+	echo "[INFO] Running $cmd"
+  if $cmd; then
+		echo [INFO] TEST $file PASSED ;
+	else
+		echo [ERROR] >&2 ;
+		echo [ERROR] TEST $file FAILED  >&2 ;
+		echo [ERROR] to repeat the test type in: >&2 ;
+		echo [ERROR]  $cmd >&2 ;
+		echo [ERROR] >&2 ;
+		rc=1
+	fi
+done
+exit $rc
+
